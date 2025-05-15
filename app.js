b0f5442e548d6995303aa3bef96890bca48d7004
ahhkhoa56 <anhkhoa.wm@gmail.com>
2025-05-15 14:40:50 +0000

    send emial with template

--- app.js
+++ app.js
@@ -14,7 +14,7 @@ var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var authRouter = require('./routes/auth');
 var eventRouter = require('./routes/event');
-// var mailRouter = require('./routes/mail');
+var mailRouter = require('./routes/mail');
 var db = require('./db/db.js');
 
 
@@ -46,7 +46,7 @@ app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/auth', authRouter);
 app.use('/event', eventRouter);
-// app.use('/mail', mailRouter);
+app.use('/mail', mailRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
--- routes/mail.js
+++ routes/mail.js
@@ -1,31 +1,53 @@
-// var express = require('express');
-// var router = express.Router();
+var express = require('express');
+var router = express.Router();
+var ejs = require('ejs');
+var path = require('path');
 
-// var nodemailer = require('nodemailer');
-// let transporter = nodemailer.createTransport( {
-//     host: 'smtp.gmail.com',
-//     port: 465,
-//     secure: true,
-//     auth: {
-//         user: "myevents.uoa@gmail.com",
-//         pass: process.env.GMAIL_PASS
-//      }
-// });
+var nodemailer = require('nodemailer');
+let transporter = nodemailer.createTransport( {
+    host: 'smtp.gmail.com',
+    port: 465,
+    secure: true,
+    auth: {
+        user: "myevents.uoa@gmail.com",
+        pass: process.env.GMAIL_PASS
+     }
+});
 
-// router.get('/', function(req, res, next) {
-//   res.render('index', { title: 'Express' });
-// });
 
-// /* Sends an email to the provided address. */
-// router.post('/email', function(req, res, next) {
-//   let info = transporter.sendMail({
-//       from: "myevents.uoa@gmail.com", // sender address
-//       to: req.body.recipient, // list of receivers
-//       subject: req.body.subject, // Subject line
-//       text: req.body.text, // plain text body
-//       html: "<b>"+req.body.text+"</b>" // html body
-//   });
-//   res.send();
-// });
+router.post('/confirmation', async (req, res, next) => {
+  try {
+    const { recipient, name, eventTitle, message } = req.body;
 
-// module.exports = router;
+    // path to EJS file
+    const templatePath = path.join(__dirname, '..', 'email_template', 'confirmation.ejs');
+
+    // renderFile returns HTML
+    const html = await ejs.renderFile(templatePath, {
+      name,
+      eventTitle,
+      message,
+      imageCid: 'eventImage'
+    });
+
+    await transporter.sendMail({
+      from: 'myevents.uoa@gmail.com',
+      to: recipient,
+      subject: `Confirmation for ${eventTitle}`,
+      html,
+      attachments: [
+        {
+          filename: 'image.jpg',
+          path: 'email_template/email_image/image.jpg',
+          cid: 'eventImage'
+        }
+      ]
+    });
+
+    res.sendStatus(200);
+  } catch (err) {
+    console.error('Email send failed:', err);
+    next(err);
+  }
+});
+module.exports = router;
