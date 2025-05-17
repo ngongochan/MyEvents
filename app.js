c1536fccf9ef6eed0d228cbc185db08ea52308de
Luong Phuoc Nguyen <a1917718@adelaide.edu.aum>
2025-05-17 09:56:39 +0000

    index + event vue interaction

--- app.js
+++ app.js
@@ -31,7 +31,6 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-
 app.use(session({
   secret: 'WDC_113',
   resave: false ,
